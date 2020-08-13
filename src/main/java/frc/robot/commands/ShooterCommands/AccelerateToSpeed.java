0eb4796d8952b987f3181042a2d2a26a68559906
shahar <66435925+spaderski@users.noreply.github.com>
2020-08-13 16:20:59 +0300

    do the commands for the shooter

---
+++ src/main/java/frc/robot/commands/ShooterCommands/AccelerateToSpeed.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.ShooterCommands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class AccelerateToSpeed extends CommandBase {
+  private final Shooter shooter;
+  private double targetSpeed;
+  private double range;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param shooter The subsystem used by this command.
+   */
+  public AccelerateToSpeed(Shooter s, double target, double range) {
+    shooter = s;
+    targetSpeed = target;
+    this.range = range;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(s);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    shooter.setVelocityTarget(targetSpeed);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  int counter = 0;
+  @Override
+  public boolean isFinished() {
+    if ( Math.abs(targetSpeed - shooter.getVelocity()) <= range){
+        counter ++;
+    }
+    else {
+        counter = 0;
+    }
+    return counter > 5;
+  }
+}
