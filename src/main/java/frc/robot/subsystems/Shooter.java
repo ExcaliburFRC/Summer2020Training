03f342e45cd3b5dcee4fd76292be0ff270c8af21
shahar <66435925+spaderski@users.noreply.github.com>
2020-08-13 12:26:50 +0300

    fix the problam

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,13 +1,21 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-  
-  public Shooter() {
-    
+  CANSparkMax motor;
+
+  public Shooter(CANSparkMax m){
+    this.motor = m;
   }
 
+  public void setSpeed(double speed) {
+    motor.set(speed);
+  }
+
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
