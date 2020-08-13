e58d86c60d21590763916d6fa7aa9e03ae2b8b71
shahar <66435925+spaderski@users.noreply.github.com>
2020-08-13 12:47:40 +0300

    puctions getSpeed setSpeed getEncoderCPR and adding encoder and put the encoder in the smartdashboard

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,23 +1,35 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   CANSparkMax motor;
+  CANEncoder encoder;
 
-  public Shooter(CANSparkMax m){
+
+  public Shooter(CANSparkMax m) {
     this.motor = m;
+    this.encoder = m.getEncoder();
   }
 
   public void setSpeed(double speed) {
     motor.set(speed);
   }
 
-  
+  public double getSpeed() {
+    return motor.get();
+  }
+
+  public int getEncoderCPR(){
+    return encoder.getCountsPerRevolution();
+  }
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("CPR", getEncoderCPR());
   }
 }
