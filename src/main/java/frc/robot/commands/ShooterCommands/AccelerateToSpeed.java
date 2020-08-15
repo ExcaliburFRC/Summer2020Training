25bd831e41c9d8fcd89ccf14ecd60287e9a4be0b
spaderski <66435925+spaderski@users.noreply.github.com>
2020-08-16 01:26:37 +0300

    [commands.shooter] AccelerateToSpeed (#12)

--- src/main/java/frc/robot/commands/ShooterCommands/AccelerateToSpeed.java
+++ src/main/java/frc/robot/commands/ShooterCommands/AccelerateToSpeed.java
@@ -5,45 +5,56 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.robot.commands.ShooterCommands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
- * An example command that uses an example subsystem.
+ * An AccelerateToSpeed command that uses a shooter subsystem.
  */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+public class AccelerateToSpeed extends CommandBase {
+  private final Shooter shooter;
+  private double targetSpeed;
+  private double range;
 
   /**
-   * Creates a new ExampleCommand.
+   * Creates a new AccelerateToSpeed.
    *
-   * @param subsystem The subsystem used by this command.
+   * @param shooter The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public AccelerateToSpeed(Shooter s, double target, double range) {
+    shooter = s;
+    targetSpeed = target;
+    this.range = range;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(s);
   }
-
+  
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    shooter.setVelocityTarget(targetSpeed);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
   }
-  
+
   // Returns true when the command should end.
+  int counter = 0;
   @Override
   public boolean isFinished() {
-    return false;
+    if ( Math.abs(targetSpeed - shooter.getVelocity()) <= range){
+        counter ++;
+    }
+    else {
+        counter = 0;
+    }
+    return counter > 5;
   }
-  
+
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
